fa8a1e7b2d047aa5575bed12cf05b187fc2dc01a
gladja <gladkih.alexander@gmail.com>
2024-02-15 22:29:57 +0200

    add routing, add component MyAdvertisements, add page Advertisements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { Layout } from 'components/Layout';
 import Home from 'pages/Home';
 import { GlobalStyle, Theme } from '../styles';
 import { Container } from './Container';
+import { Advertisements } from 'pages/Advertisements';
 
 function App() {
   return (
@@ -10,7 +11,14 @@ function App() {
       <Container>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route path="/home" element={<Home />} />
+            {/* <Route path="/home" element={<Home />} /> */}
+            <Route path="/advertisements" element={<Advertisements />}>
+              <Route path="active" element={'active'} />
+              <Route path="awaiting" element={'awaiting'} />
+              <Route path="delivery" element={'delivery'} />
+              <Route path="inactive" element={'inactive'} />
+              <Route path="declined" element={'declined'} />
+            </Route>
             {/* <Route path="*" element={} /> */}
           </Route>
         </Routes>
---
+++ src/components/MyAdvertisements/index.js
@@ -0,0 +1,1 @@
+export { MyAdvertisements } from './MyAdvertisements';
