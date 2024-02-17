823de76792cadca5c10474d636bf9a2c8127e872
Mari <mari.philon@gmail.com>
2024-02-17 13:29:32 +0200

    add: header, search, styles

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,11 +2,13 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../Header';
 import { UserMenu } from '../UserMenu';
+import { SearchBox } from '../Search/SearchBox';
 
 export const Layout = () => {
   return (
     <>
       <Header />
+      <SearchBox />
       <UserMenu />
       <Suspense fallback={null}>
         <Outlet />
--- src/styles/Theme.jsx
+++ src/styles/Theme.jsx
@@ -5,12 +5,96 @@ import { ThemeProvider } from 'styled-components';
 // `;
 
 const theme = {
-  colors: {
-    black: '#121417',
-    grey: '#8a8a89',
-    white: '#fff',
+  palette: {
+    primary: {
+      background: '#FFFFFF',
+      error: '#B00020'
+    },
+    grey: {
+      900: '#2B2D42',
+      800: '#3F415A',
+      700: '#505370',
+      600: '#626687',
+      500: '#717498',
+      400: '#8688A9',
+      300: '#9B9DBC',
+      200: '#B7B9D3',
+      100: '#D1D5EB',
+      50: '#EFECFF'
+    },
+    orange: {
+      main: '#FF6F00',
+      800: '#FF8F00',
+      700: '#FFA000',
+      600: '#FFB300',
+      500: '#FFC106',
+      400: '#FFCA28',
+      300: '#FFD54F',
+      200: '#FFE082',
+      100: '#FFECB3',
+      50: '#FFF8E1'
+    },
   },
-};
+  typography: {
+    fontFamily: 'Montserrat',
+    h1: {
+      fontFamily: 'Montserrat',
+      fontWeight: 'Regular',
+      fontSize: 36
+    },
+    h2: {
+      fontFamily: 'Montserrat',
+      fontWeight: 'Regular',
+      fontSize: 24
+    },
+    h3: {
+      fontFamily: 'Montserrat',
+      fontWeight: 'SemiBold',
+      fontSize: 22
+    },
+    h4: {
+      fontFamily: 'Inter',
+      fontWeight: 'Regular',
+      fontSize: 22
+    },
+    h5: {
+      fontFamily: 'Montserrat',
+      fontWeight: 'Regular',
+      fontSize: 18
+    },
+    h6: {
+      fontFamily: 'Inter',
+      fontWeight: 'Regular',
+      fontSize: 18
+    },
+    p1: {
+      fontFamily: 'Inter',
+      fontWeight: 'Regular',
+      fontSize: 14
+    },
+    p2: { 
+      fontFamily: 'Inter',
+      fontWeight: 'Regular',
+      fontSize: 12
+    },
+    p3: {
+      fontFamily: 'Inter',
+      fontWeight: 'Regular',
+      fontSize: 10
+    },
+    small: {
+      fontFamily: 'Montserrat',
+      fontWeight: 'Regular',
+      fontSize: 8
+    },
+    tags: {
+      fontFamily: 'Inter',
+      fontWeight: 'Regular',
+      fontSize: 14,
+      textTransform: 'uppercase'
+    },
+  }
+}
 
 const Theme = ({ children }) => (
   <ThemeProvider theme={theme}>{children}</ThemeProvider>
